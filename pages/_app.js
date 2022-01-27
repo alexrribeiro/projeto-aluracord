cf7135a99f30840d13f5f3ae1b27200543e06ad5
alexrribeiro <alexramosribeiro@gmail.com>
2022-01-26 22:13:47 -0300

    Aula III - Chat Offline

--- pages/_app.js
+++ pages/_app.js
@@ -30,7 +30,7 @@ function GlobalStyle() {
 }
 
 export default function CustomApp({ Component, pageProps }) {
-  console.log('Rodando em todas as páginas!!!')
+  // console.log('Rodando em todas as páginas!!!')
   return (
     <>
       <GlobalStyle />
